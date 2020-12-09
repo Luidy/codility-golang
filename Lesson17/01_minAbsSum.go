e2749b59defa33f85b70edfbc41c4685cdc3492d
Luidy <ejjo321@gmail.com>
2020-12-09 22:51:05 +0900

    add. MinAbsSum 100% score

--- Lesson17/01_minAbsSum.go
+++ Lesson17/01_minAbsSum.go
@@ -16,27 +16,33 @@ func abs(a int) int {
 func Solution(A []int) int {
     // write your code in Go 1.4
     maxLen := 0
-    countNum := make(map[int]int)
+    countNum := make([]int, 101)
     for _, a := range A {
         absNum := abs(a)
         countNum[absNum] = countNum[absNum] + 1
         maxLen = maxLen + absNum
     }
 
-    subset := make(map[int]int)
+    subset := make([]int, maxLen/2+1)
     subset[0] = 1
-    for k, v := range countNum {
+    for x, v := range countNum {
+        if v == 0 {
+            continue
+        }
         for i:=0; i<maxLen/2+1; i++ {
             if subset[i] > 0 {
                 subset[i] = v+1
-            } else if (i >= k && subset[i-k] > 1) {
-                subset[i] = subset[i-k] - 1
+            } else if (i >= x && subset[i-x] > 1) {
+                subset[i] = subset[i-x] - 1
             }
         }
     }
 
     result := maxLen
-    for k, _ := range subset {
+    for k, s := range subset {
+        if s == 0 {
+            continue
+        }
         if result > maxLen-k*2 {
             result =  maxLen-k*2
         }
