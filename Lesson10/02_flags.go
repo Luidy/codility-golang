4d1e1da902aaccb2af75cc24bff10205429fa44a
Luidy <ejjo321@gmail.com>
2020-12-17 22:53:09 +0900

    add. Flags 26% score

---
+++ Lesson10/02_flags.go
@@ -0,0 +1,22 @@
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+// import "os"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func Solution(A []int) int {
+	// write your code in Go 1.4
+	var peak []int
+	for i := 1; i < len(A)-1; i++ {
+		if A[i-1] < A[i] && A[i] > A[i+1] {
+			peak = append(peak, i)
+		}
+	}
+	if len(peak) <= 2 {
+		return len(peak)
+	}
+	return 0
+}
