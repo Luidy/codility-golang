110405a4f8ff18cd44e946b95f08a3c23ea64fc9
Luidy <ejjo321@gmail.com>
2020-12-21 08:49:46 +0900

    add. Flags 100% score

--- Lesson10/02_flags.go
+++ Lesson10/02_flags.go
@@ -9,33 +9,44 @@ package solution
 
 func Solution(A []int) int {
 	// write your code in Go 1.4
-	var peak []int
+	peak := make([]int, len(A))
+	peakCnt := 0
 	for i := 1; i < len(A)-1; i++ {
 		if A[i-1] < A[i] && A[i] > A[i+1] {
-			peak = append(peak, i)
+			peak[i] = 1
+			peakCnt++
 		}
 	}
-	if len(peak) <= 2 {
-		return len(peak)
+	if peakCnt <= 2 {
+		return peakCnt
 	}
+	next := make([]int, len(A))
+	for i := len(A) - 2; i >= 0; i-- {
+		if peak[i] == 1 {
+			next[i] = i
+		} else {
+			next[i] = next[i+1]
+		}
+	}
+
 	flag := 0
-	for i := len(peak); i > 1; i-- {
-		curFlag := 1
-		curPos := peak[0]
-		for x := 1; x < len(peak); x++ {
-			if peak[x]-curPos >= i {
-				curFlag++
-				curPos = peak[x]
-			} else {
-				continue
-			}
-			if i == curFlag {
+	i := 1
+	for (i-1)*i <= len(A) {
+		curFlag := 0
+		curPos := 0
+		for curFlag < i && curPos < len(A) {
+			curPos = next[curPos]
+			if curPos == 0 {
 				break
 			}
+			curFlag++
+			curPos = curPos + i
 		}
+
 		if flag < curFlag {
 			flag = curFlag
 		}
+		i++
 	}
 	return flag
 }
