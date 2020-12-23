b7bd125920ddb48638e6cf398a77342241940f1d
Luidy <ejjo321@gmail.com>
2020-12-23 09:46:55 +0900

    add. CountSemiprimes 100% score

--- Lesson11/02_countSemiprimes.go
+++ Lesson11/02_countSemiprimes.go
@@ -1,7 +1,7 @@
 package solution
 
 // you can also use imports, for example:
-import "fmt"
+// import "fmt"
 
 // import "os"
 
@@ -12,7 +12,7 @@ func Solution(N int, P []int, Q []int) []int {
 	// write your code in Go 1.4
 	prime := make([]int, N+1)
 	i := 2
-	for i*i < N {
+	for i*i <= N {
 		if prime[i] == 0 {
 			k := i * i
 			for k <= N {
@@ -24,7 +24,22 @@ func Solution(N int, P []int, Q []int) []int {
 		}
 		i++
 	}
-	fmt.Printf("%v", prime)
+	semiprime := make([]int, N+1)
+	for i := 1; i < len(prime); i++ {
+		p := prime[i]
+		if p == 0 {
+			semiprime[i] = semiprime[i-1]
+			continue
+		}
+		if prime[i/p] == 0 {
+			semiprime[i] = semiprime[i-1] + 1
+		} else {
+			semiprime[i] = semiprime[i-1]
+		}
+	}
 	result := make([]int, len(P))
+	for i, p := range P {
+		result[i] = semiprime[Q[i]] - semiprime[p-1]
+	}
 	return result
 }
