887a9fcce4375b2e45403282419f9182d1c65ea3
Luidy <ejjo321@gmail.com>
2020-12-29 18:50:27 +0900

    add. NailingPlanks find midIndex logic

--- Lesson14/02_nailingPlanks.go
+++ Lesson14/02_nailingPlanks.go
@@ -12,6 +12,27 @@ import (
 // fmt.Println("this is a debug message")
 type Matrix [][]int
 
+func getMinIndex(start, end int, nail Matrix, index int) int {
+	min := 0
+	max := nail.Len() - 1
+	minIndex := -1
+	for min <= max {
+		mid := (min + max) / 2
+		if nail[mid][0] < start {
+			min = mid + 1
+		} else if nail[mid][0] > end {
+			max = mid - 1
+		} else {
+			max = mid - 1
+			minIndex = mid
+		}
+	}
+	if minIndex == -1 {
+		return -1
+	}
+	return minIndex
+}
+
 func (m Matrix) Len() int           { return len(m) }
 func (m Matrix) Less(i, j int) bool { return m[i][0] < m[j][0] }
 func (m Matrix) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
@@ -27,6 +48,11 @@ func Solution(A []int, B []int, C []int) int {
 		sortNail[i][1] = i
 	}
 	sort.Sort(sortNail)
-	fmt.Printf("%v", sortNail)
+	fmt.Printf("%v \n", sortNail)
+	result := 0
+	for i := 0; i < len(A); i++ {
+		result = getMinIndex(A[i], B[i], sortNail, result)
+		fmt.Printf("A %d B %d i %d, minIndex %d \n", A[i], B[i], i, result)
+	}
 	return -1
 }
