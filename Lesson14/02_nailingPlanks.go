564f9b331d6b3ff0fdce93a750614539d60bfc00
Luidy <ejjo321@gmail.com>
2020-12-30 14:17:47 +0900

    NailingPlanks 100% score

--- Lesson14/02_nailingPlanks.go
+++ Lesson14/02_nailingPlanks.go
@@ -1,71 +1,41 @@
 package solution
 
 // you can also use imports, for example:
-import (
-	// "fmt"
-	"sort"
-)
-
+// import "fmt"
 // import "os"
 
 // you can write to stdout for debugging purposes, e.g.
 // fmt.Println("this is a debug message")
-type Matrix [][]int
-
-func getMinIndex(start, end int, nail Matrix, index int) int {
-	min := 0
-	max := len(nail) - 1
-	minIndex := -1
-	for min <= max {
-		mid := (min + max) / 2
-		if nail[mid][0] < start {
-			min = mid + 1
-		} else if nail[mid][0] > end {
-			max = mid - 1
-		} else {
-			max = mid - 1
-			minIndex = mid
-		}
-	}
-	if minIndex == -1 {
-		return -1
-	}
-	minIndexOrigin := nail[minIndex][1]
-	for i := minIndex; i < len(nail); i++ {
-		if nail[i][0] > end {
-			break
-		}
-		if minIndexOrigin > nail[i][1] {
-			minIndexOrigin = nail[i][1]
-		}
-		if minIndexOrigin <= index {
-			return index
-		}
-	}
-	return minIndexOrigin
-}
-
-func (m Matrix) Len() int           { return len(m) }
-func (m Matrix) Less(i, j int) bool { return m[i][0] < m[j][0] }
-func (m Matrix) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
 
 func Solution(A []int, B []int, C []int) int {
 	// write your code in Go 1.4
-	sortNail := make(Matrix, len(C))
-	for i := 0; i < len(C); i++ {
-		sortNail[i] = make([]int, 2)
-	}
-	for i, c := range C {
-		sortNail[i][0] = c
-		sortNail[i][1] = i
-	}
-	sort.Sort(sortNail)
-	result := 0
-	for i := 0; i < len(A); i++ {
-		result = getMinIndex(A[i], B[i], sortNail, result)
-		if result == -1 {
-			return -1
+	min := 1
+	max := len(C)
+	nail := make([]int, len(C)*2+1)
+	total := -1
+	for min <= max {
+		miss := false
+		mid := (min + max) / 2
+		for i := 0; i < len(nail); i++ {
+			nail[i] = 0
+		}
+		for i := 0; i < mid; i++ {
+			nail[C[i]]++
+		}
+		for i := 1; i < len(nail); i++ {
+			nail[i] = nail[i] + nail[i-1]
+		}
+		for i := 0; i < len(A); i++ {
+			if nail[A[i]-1] == nail[B[i]] {
+				miss = true
+			}
+		}
+		if miss {
+			min = mid + 1
+		} else {
+			max = mid - 1
+			total = mid
 		}
 	}
-	return result + 1
+	return total
 }
